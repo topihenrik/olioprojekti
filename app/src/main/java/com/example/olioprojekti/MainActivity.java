afd01d25670ed34dcaaf257911c2d02c34a40be9
Topi Ilmoni <topi.ilmoni@gmail.com>
2021-04-17 20:25:56 +0300

    Fixed a typo in AndroidManifest.xml

--- app/src/main/java/com/example/olioprojekti/MainActivity.java
+++ app/src/main/java/com/example/olioprojekti/MainActivity.java
@@ -6,17 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
 
 public class MainActivity extends AppCompatActivity {
 
