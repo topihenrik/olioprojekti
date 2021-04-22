000b8f8fdc2e469b613170cd98c5d26861ad50ca
Topi Ilmoni <topi.ilmoni@gmail.com>
2021-04-22 17:08:01 +0300

    Api fixed

--- app/src/main/java/com/example/olioprojekti/SwaggerApi.java
+++ app/src/main/java/com/example/olioprojekti/SwaggerApi.java
@@ -2,14 +2,17 @@ package com.example.olioprojekti;
 
 import android.util.Log;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -19,8 +22,10 @@ public class SwaggerApi {
     public void SwaggerApi() {
         return;
     }
+    Gson gson = new Gson();
+    static ArrayList<String> arrayList = new ArrayList<>();
 
-    public void readXML() {
+    public void readXML2() {
         Log.d("Testi", "STRING445");
         try {
             DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
@@ -28,14 +33,9 @@ public class SwaggerApi {
             Document doc = builder.parse(urlString);
             doc.getDocumentElement().normalize();
             Log.d("String", "Kaikki ok!");
+            Type userListType = new TypeToken<ArrayList<String>>(){}.getType();
+            //arrayList = gson.fromJson(doc, userListType);
 
-            String element = doc.getElementsByTagName("Total").item(0).getTextContent();
-
-            //NodeList nodeList = doc.getElementsByTagName("Total");
-            //Node node = nodeList.item(0);
-            //Element element = (Element) node;
-
-            Log.d("Total", "* * * * * * * * * * * * *" + element.toString());
 
 
         } catch (ParserConfigurationException e) {
@@ -46,6 +46,32 @@ public class SwaggerApi {
             e.printStackTrace();
         }
         return;
+    }
+
+    public void readXML(){
+        //Instantiating the URL class
+        URL url = null;
+        try {
+            url = new URL("https://ilmastodieetti.ymparisto.fi/ilmastodieetti/calculatorapi/v1/FoodCalculator?query.diet=omnivore");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        //Retrieving the contents of the specified page
+        Scanner sc = null;
+        try {
+            sc = new Scanner(url.openStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //Instantiating the StringBuffer class to hold the result
+        StringBuffer sb = new StringBuffer();
+        while(sc.hasNext()) {
+            sb.append(sc.next());
+            //System.out.println(sc.next());
+        }
+        //Retrieving the String from the String Buffer object
+        String result = sb.toString();
+        Log.d("Contents: ", result);
     }
 
 }
